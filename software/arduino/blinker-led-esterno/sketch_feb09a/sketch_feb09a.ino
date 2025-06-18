46b49d1f7a53200824831d7292377b6f2be8dcde
the Linux Nerd <fabio.linux.nerd@gmail.com>
2025-06-18 12:05:38 +0200

    aggiunto blinker led esterno

--- software/arduino/blinker-led-esterno/sketch_feb09a/sketch_feb09a.ino
+++ software/arduino/blinker-led-esterno/sketch_feb09a/sketch_feb09a.ino
@@ -5,6 +5,20 @@
  * con questo programma colleghiamo per la prima volta un componente esterno ad Arduino,
  * il mio suggerimento è di utilizzare una piccola breadboard come si può vedere nel file
  * Fritzing allegato al progetto
+ *
+ * la resistenza da applicare al led è calcolata in base alla legge di Ohm partendo dai
+ * seguenti dati:
+ *
+ * - Arduino eroga 5v e 40mA
+ * - il led rosso lavora a 1,8v e 15mA
+ *
+ * sulla resistenza avremo una caduta di tensione di 3,2v (5v di Arduino meno 1,8v del led)
+ * quindi usiamo la legge di Ohm (R = V / I) per trovare una resistenza che faccia
+ * passare 15mA e 3v quindi avremo R = 3V / 0,015A = 200Ohm; la resistenza commerciale che
+ * si avvicina di più a 200Ohm è quella da 220Ohm, che è quindi quella che impiegheremo
+ *
+ * vedi anche:
+ * - https://www.elettrograf.com/il-led-note-tecniche/
  * 
  */
 
